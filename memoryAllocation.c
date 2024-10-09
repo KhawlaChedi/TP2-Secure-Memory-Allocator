3be91a484f4a4a1a44117c82f4c3b452cf72cb14
KhawlaChedi <khawlachedi11@gmail.com>
2024-10-09 23:15:22 +0200

    Cutting large blocks

--- memoryAllocation.c
+++ memoryAllocation.c
@@ -12,6 +12,7 @@ typedef struct HEADER_TAG {
 #define MAGIC_NUMBER 0x0123456789ABCDEFL
 #define HEADER_SIZE sizeof(HEADER)
 #define MAGIC_NUMBER_SIZE sizeof(long)
+#define MINIMUM_BLOCK_SIZE (HEADER_SIZE + 1 + MAGIC_NUMBER_SIZE) 
 
 HEADER *free_list = NULL;  
 
@@ -39,15 +40,14 @@ void sort_free_list_by_size() {
     free_list = sorted;
 }
 
-
 HEADER *find_compatible_block(size_t size) {
-    sort_free_list_by_size();  
+    sort_free_list_by_size();
 
     HEADER *prev = NULL;
     HEADER *current = free_list;
 
     while (current) {
-        if (current->block_size >= size) {
+        if (current->block_size == size) {
             if (prev) {
                 prev->ptr_next = current->ptr_next;
             } else {
@@ -55,59 +55,78 @@ HEADER *find_compatible_block(size_t size) {
             }
             return current;
         }
+
+        if (current->block_size > size) {
+            size_t remaining_size = current->block_size - size - HEADER_SIZE - MAGIC_NUMBER_SIZE;
+
+            if (remaining_size >= MINIMUM_BLOCK_SIZE) {
+                HEADER *new_block = (HEADER*)((char*)current + HEADER_SIZE + size + MAGIC_NUMBER_SIZE);
+                new_block->block_size = remaining_size;
+                new_block->magic_number = MAGIC_NUMBER;
+
+                new_block->ptr_next = current->ptr_next;
+                current->block_size = size;
+
+                if (prev) {
+                    prev->ptr_next = new_block;
+                } else {
+                    free_list = new_block;
+                }
+
+                return current;
+            }
+        }
+
+
         prev = current;
         current = current->ptr_next;
     }
-    
+
     return NULL;
 }
+
 
 void *malloc_3is(size_t size) {
     size_t total_size = HEADER_SIZE + size + MAGIC_NUMBER_SIZE;
 
-    
-    HEADER *header = find_compatible_block(size);
+    HEADER *header = find_compatible_block(size);  
 
     if (!header) {
-        
         void *memory_block = sbrk(total_size);
         
 
-        header = (HEADER*) memory_block;
+        header = (HEADER*)memory_block;
         header->block_size = size;
         header->magic_number = MAGIC_NUMBER;
-        header->ptr_next = NULL;  
+        header->ptr_next = NULL;
     }
-    
+
+
     void *user_memory = (void*)(header + 1);  
-    
     long *footer_magic = (long*)((char*)user_memory + size);  
     *footer_magic = MAGIC_NUMBER;
-    
+
     return user_memory;
 }
 
+
 int check_memory_overflow(void *base_ptr, void *element_ptr) {
     HEADER *header = (HEADER*)((char*)base_ptr - HEADER_SIZE);
-    
     
     if (header->magic_number != MAGIC_NUMBER) {
         printf("Erreur : modification du magic number avant l'en-tête !\n");
         return -1;
     }
 
-    
     long *footer_magic = (long*)((char*)base_ptr + header->block_size);
     if (*footer_magic != MAGIC_NUMBER) {
         printf("Erreur : modification du magic number après le bloc !\n");
         return -1;
     }
 
-    
     char *start_address = (char *)header + HEADER_SIZE; 
     char *end_address = start_address + header->block_size; 
 
-    
     if (element_ptr < (void *)start_address || element_ptr >= (void *)end_address) {
         printf("Erreur : accès hors limites à l'adresse %p\n", element_ptr);
         return -1;
@@ -136,50 +155,35 @@ int get_free_list_size() {
 }
 
 int main() {
-    int *tab = (int *)malloc_3is(2 * sizeof(int)); 
+    int *tab = (int *)malloc_3is(4 * sizeof(int)); 
 
-    tab[0] = 10;
-    if (check_memory_overflow(tab, &tab[0]) == -1) exit(EXIT_FAILURE);
+    tab[0] = 10;    
     tab[1] = 20;
-    if (check_memory_overflow(tab, &tab[1]) == -1) exit(EXIT_FAILURE);
+    tab[2] = 30;    
+    tab[3] = 40;
     
     printf("Values of the array:\n");
     printf("Element 0: %d\n", tab[0]);
     printf("Element 1: %d\n", tab[1]);
-    
-/*    //change magic number test
-    tab[2] = 30; 
-    if (check_memory_overflow(tab, &tab[2]) == -1) exit(EXIT_FAILURE); 
-     tab[-2] = 30; 
-    if (check_memory_overflow(tab, &tab[-2]) == -1) exit(EXIT_FAILURE);  
-    
-    //access test
-    for (int i = 1; i >= -1; i--) {
-        printf("Element %d: %d\n", i, tab[i]);
-        if (check_memory_overflow(tab, &tab[i]) == -1) exit(EXIT_FAILURE);
-    } */
+    printf("Element 0: %d\n", tab[2]);
+    printf("Element 1: %d\n", tab[3]);
 
     printf("Size of free_list before freeing: %d\n", get_free_list_size());
-
     free_3is(tab);
     printf("Memory freed.\n");
-
     printf("Size of free_list after freeing: %d\n", get_free_list_size());
     
     int *new_tab = (int *)malloc_3is(2 * sizeof(int));  
-    new_tab[0] = 30;
-    new_tab[1] = 40;
+    new_tab[0] = 50;
+    new_tab[1] = 60;
     
-
     printf("New array values after reusing freed block:\n");
     printf("Element 0: %d\n", new_tab[0]);
     printf("Element 1: %d\n", new_tab[1]);
     
-    printf("Size of free_list before freeing: %d\n", get_free_list_size());
-    
+    printf("Size of free_list before freeing: %d\n", get_free_list_size());   
     free_3is(new_tab);
     printf("Memory freed.\n");
-
     printf("Size of free_list after freeing: %d\n", get_free_list_size());
 
     return 0;
