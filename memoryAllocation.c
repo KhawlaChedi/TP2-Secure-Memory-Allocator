3321a7a37e378087e563a6ad295e785e70d88ea8
KhawlaChedi <khawlachedi11@gmail.com>
2024-10-09 11:25:00 +0200

    Bloc free

--- memoryAllocation.c
+++ memoryAllocation.c
@@ -12,6 +12,8 @@ typedef struct HEADER_TAG {
 #define HEADER_SIZE sizeof(HEADER)
 #define MAGIC_NUMBER_SIZE sizeof(long)
 
+HEADER *free_list = NULL;  
+
 void *malloc_3is(size_t size) {
    
     size_t total_size = HEADER_SIZE + size + MAGIC_NUMBER_SIZE;
@@ -30,21 +32,46 @@ void *malloc_3is(size_t size) {
     return user_memory;
 }
 
+
+void free_3is(void *ptr) {
+    if (!ptr) return;
+    HEADER *header = (HEADER*)((char*)ptr - HEADER_SIZE); 
+
+   
+    header->ptr_next = free_list;  
+    free_list = header;             
+}
+
+int get_free_list_size() {
+    int count = 0;
+    HEADER *current = free_list;
+
+    while (current) {
+        count++;
+        current = current->ptr_next;
+    }
+    return count;
+}
+
 int main(){
 
+   
     int *tab = (int *)malloc_3is(2 * sizeof(int)); 
-
+ /* printf("Adresse du tableau: %p\n", (void *)tab); */
 
     tab[0] = 10;
     tab[1] = 20; 
 
-
     printf("Valeurs du tableau :\n");
     printf("Element 0: %d\n", tab[0]);
     printf("Element 1: %d\n", tab[1]);
+    
+    printf("Taille de free_list avant libération: %d\n", get_free_list_size());
 
-  
-  /*  printf("Adresse du tableau: %p\n", (void *)tab); */
+    free_3is(tab);
+    printf("Mémoire libérée.\n");
+
+    printf("Taille de free_list après libération: %d\n", get_free_list_size());
     
     return 0;
 }
