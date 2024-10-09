8193789df4b16458374a2a6edda82f1fc741020b
KhawlaChedi <khawlachedi11@gmail.com>
2024-10-09 19:43:24 +0200

    Overflow check

--- memoryAllocation.c
+++ memoryAllocation.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>  
 
 typedef struct HEADER_TAG {
     struct HEADER_TAG *ptr_next;  
-    size_t bloc_size;             
+    size_t block_size;             
     long magic_number;           
 } HEADER;
 
@@ -15,13 +16,12 @@ typedef struct HEADER_TAG {
 HEADER *free_list = NULL;  
 
 void *malloc_3is(size_t size) {
-   
     size_t total_size = HEADER_SIZE + size + MAGIC_NUMBER_SIZE;
     
     void *memory_block = sbrk(total_size);
-  
+    
     HEADER *header = (HEADER*) memory_block;
-    header->bloc_size = size;
+    header->block_size = size;
     header->magic_number = MAGIC_NUMBER;
     header->ptr_next = NULL;  
         
@@ -32,12 +32,36 @@ void *malloc_3is(size_t size) {
     return user_memory;
 }
 
+int check_memory_overflow(void *base_ptr, void *element_ptr) {
+
+    HEADER *header = (HEADER*)((char*)base_ptr - HEADER_SIZE);   
+    if (header->magic_number != MAGIC_NUMBER) {
+        printf("Error: magic number modified before the bloc!\n");
+        return -1;
+    }
+
+   
+    long *footer_magic = (long*)((char*)base_ptr + header->block_size);
+    if (*footer_magic != MAGIC_NUMBER) {
+        printf("Error: magic number modified after the block!\n");
+        return -1;
+    }
+
+    
+    char *start_address = (char *)header + HEADER_SIZE; 
+    char *end_address = start_address + header->block_size; 
+    if (element_ptr < (void *)start_address || element_ptr >= (void *)end_address) {
+        printf("Error: out-of-bounds access at address %p\n", element_ptr);
+        return -1;
+    }
+
+    return 0;  
+}
 
 void free_3is(void *ptr) {
     if (!ptr) return;
     HEADER *header = (HEADER*)((char*)ptr - HEADER_SIZE); 
 
-   
     header->ptr_next = free_list;  
     free_list = header;             
 }
@@ -53,25 +77,36 @@ int get_free_list_size() {
     return count;
 }
 
-int main(){
-
-   
+int main() {
     int *tab = (int *)malloc_3is(2 * sizeof(int)); 
- /* printf("Adresse du tableau: %p\n", (void *)tab); */
 
     tab[0] = 10;
-    tab[1] = 20; 
-
-    printf("Valeurs du tableau :\n");
+    if (check_memory_overflow(tab, &tab[0]) == -1) exit(EXIT_FAILURE);
+    tab[1] = 20;
+    if (check_memory_overflow(tab, &tab[1]) == -1) exit(EXIT_FAILURE);
+    
+    printf("Values of the array:\n");
     printf("Element 0: %d\n", tab[0]);
     printf("Element 1: %d\n", tab[1]);
     
-    printf("Taille de free_list avant libération: %d\n", get_free_list_size());
+    //change magic number test
+    tab[2] = 30; 
+    if (check_memory_overflow(tab, &tab[2]) == -1) exit(EXIT_FAILURE); 
+     tab[-2] = 30; 
+    if (check_memory_overflow(tab, &tab[-2]) == -1) exit(EXIT_FAILURE);  
+    
+    //access test
+    for (int i = 1; i >= -1; i--) {
+        printf("Element %d: %d\n", i, tab[i]);
+        if (check_memory_overflow(tab, &tab[i]) == -1) exit(EXIT_FAILURE);
+    }
+
+    printf("Size of free_list before freeing: %d\n", get_free_list_size());
 
     free_3is(tab);
-    printf("Mémoire libérée.\n");
+    printf("Memory freed.\n");
 
-    printf("Taille de free_list après libération: %d\n", get_free_list_size());
+    printf("Size of free_list after freeing: %d\n", get_free_list_size());
     
     return 0;
 }
